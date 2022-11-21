2c351035a9f2d0e6af2b0be928ac821ef6d59b52
GDJ58 <GDJ58@192.168.7.44>
2022-11-21 10:16:15 +0900

    vo생성

---
+++ src/main/java/vo/Cash.java
@@ -0,0 +1,47 @@
+package vo;
+
+public class Cash {
+	private int cashNo;
+	// Map타입 사용 안할시 private Category category; 필요! 
+	private int categoryNo; // FK -> INNER JOIN -> Map타입
+	private long cashPrice;
+	private String cashMemo;
+	private String updatedate;
+	private String createdate;
+	public int getCashNo() {
+		return cashNo;
+	}
+	public void setCashNo(int cashNo) {
+		this.cashNo = cashNo;
+	}
+	public int getCategoryNo() {
+		return categoryNo;
+	}
+	public void setCategoryNo(int categoryNo) {
+		this.categoryNo = categoryNo;
+	}
+	public long getCashPrice() {
+		return cashPrice;
+	}
+	public void setCashPrice(long cashPrice) {
+		this.cashPrice = cashPrice;
+	}
+	public String getCashMemo() {
+		return cashMemo;
+	}
+	public void setCashMemo(String cashMemo) {
+		this.cashMemo = cashMemo;
+	}
+	public String getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(String updatedate) {
+		this.updatedate = updatedate;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+}
---
+++ src/main/java/vo/Member.java
@@ -0,0 +1,46 @@
+package vo;
+
+public class Member {
+	private int memberNo;
+	private String memberId;
+	private String memberPw;
+	private String memberName;
+	private String updatedate;
+	private String createdate;
+	public int getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(int memberNo) {
+		this.memberNo = memberNo;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getMemberPw() {
+		return memberPw;
+	}
+	public void setMemberPw(String memberPw) {
+		this.memberPw = memberPw;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public String getUpdatedate() {
+		return updatedate;
+	}
+	public void setUpdatedate(String updatedate) {
+		this.updatedate = updatedate;
+	}
+	public String getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(String createdate) {
+		this.createdate = createdate;
+	}
+}
