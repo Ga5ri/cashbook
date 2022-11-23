05f6f12282c8d4e3f6b5069b9ed491d6e0c42bc4
GDJ58 <GDJ58@192.168.7.44>
2022-11-23 15:40:34 +0900

    CashDao 수정 및 member folder 추가

--- src/main/java/dao/CashDao.java
+++ src/main/java/dao/CashDao.java
@@ -6,69 +6,74 @@ import util.*;
 
 
 public class CashDao {
-	// 호출 : cashList.jsp
-	public ArrayList<HashMap<String, Object>> selectCashListByMonth(String memberId, int year, int month) throws Exception {
-		ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String,Object>>();
+	public ArrayList<HashMap<String, Object>> slectCashListByMonth(String memberId, int year, int month) throws Exception {
+		ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+		
+		// 1. DB 연결
 		DBUtil dbUtil = new DBUtil();
 		Connection conn = dbUtil.getConnection();
+		
 		String sql = "SELECT c.cash_no cashNo, c.cash_date cashDate, c.cash_price cashPrice, c.category_no categoryNo, ct.category_kind categoryKind, ct.category_name categoryName FROM cash c INNER JOIN category ct ON c.category_no = ct.category_no WHERE c.member_id = ? AND YEAR(c.cash_date) = ? AND MONTH(c.cash_date) = ? ORDER BY c.cash_date ASC, ct.category_kind ASC";
 		PreparedStatement stmt = conn.prepareStatement(sql);
 		stmt.setString(1, memberId);
-		stmt.setInt(2, year);
-		stmt.setInt(3, month);
-		System.out.println(month+"cashlistmonth");
+		stmt.setInt(2,year);
+		stmt.setInt(3,month);
 		ResultSet rs = stmt.executeQuery();
-		System.out.println(rs+"<-rs실행여부1");
 		while(rs.next()) {
 			HashMap<String, Object> m = new HashMap<String, Object>();
-			System.out.println(rs.getInt("cashNo")+"<-cashNo"); 
+			
+			// 해당 월의 cash 정보 HashMap에 저장
 			m.put("cashNo", rs.getInt("cashNo"));
 			m.put("cashDate", rs.getString("cashDate"));
 			m.put("cashPrice", rs.getLong("cashPrice"));
 			m.put("categoryNo", rs.getInt("categoryNo"));
 			m.put("categoryKind", rs.getString("categoryKind"));
 			m.put("categoryName", rs.getString("categoryName"));
+			
+			// 리스트에 넣음
 			list.add(m);
 		}
 		
 		rs.close();
 		stmt.close();
 		conn.close();
-		return list;
+		return list;		
 	}
-	// 호출 : cashDateList.jsp
-	public ArrayList<HashMap<String, Object>> selectCashListByDate(String memberId, int year, int month, int date) throws Exception {
-		ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String,Object>>();
+	
+	public ArrayList<HashMap<String, Object>> slectCashListByDate(String memberId, int year, int month, int date) throws Exception {
+		ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+		
+		// 1. DB 연결
 		DBUtil dbUtil = new DBUtil();
 		Connection conn = dbUtil.getConnection();
+		
 		String sql = "SELECT c.cash_no cashNo, c.cash_date cashDate, c.cash_price cashPrice, c.category_no categoryNo, ct.category_kind categoryKind, ct.category_name categoryName, c.cash_memo cashMemo FROM cash c INNER JOIN category ct ON c.category_no = ct.category_no WHERE c.member_id = ? AND YEAR(c.cash_date) = ? AND MONTH(c.cash_date) = ? AND DAY(c.cash_date) = ? ORDER BY c.cash_date ASC, ct.category_kind ASC";
 		PreparedStatement stmt = conn.prepareStatement(sql);
 		stmt.setString(1, memberId);
-		System.out.println(memberId+"id");
-		stmt.setInt(2, year);
-		System.out.println(year+"year");
-		stmt.setInt(3, month);
-		System.out.println(month+"month");
-		stmt.setInt(4, date);
-		System.out.println(date+"date");
+		stmt.setInt(2,year);
+		stmt.setInt(3,month);
+		stmt.setInt(4,date);
 		ResultSet rs = stmt.executeQuery();
-		System.out.println(rs+"<-rs실행여부");
-		System.out.println(rs.next()+"<-rsnext");
 		while(rs.next()) {
 			HashMap<String, Object> m = new HashMap<String, Object>();
+			
+			System.out.println(rs.getInt("cashNo"));
+			// 해당 월의 cash 정보 HashMap에 저장
 			m.put("cashNo", rs.getInt("cashNo"));
 			m.put("cashDate", rs.getString("cashDate"));
 			m.put("cashPrice", rs.getLong("cashPrice"));
 			m.put("categoryNo", rs.getInt("categoryNo"));
-			m.put("cashMemo", rs.getLong("cashMemo"));
 			m.put("categoryKind", rs.getString("categoryKind"));
 			m.put("categoryName", rs.getString("categoryName"));
+			m.put("cashMemo", rs.getString("cashMemo"));
+			
+			// 리스트에 넣음
 			list.add(m);
 		}
 		
 		rs.close();
 		stmt.close();
 		conn.close();
-		return list;
+		return list;		
 	}
 }
